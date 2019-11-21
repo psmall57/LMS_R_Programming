10627c9b669090e5e919bef496ea710e107e7f42
psmall57 <phillip.small@gmail.com>
2019-11-21 11:16:25 -0500

    Create R_Prog_Princ.r

---
+++ R_Prog_Princ.r
@@ -0,0 +1,35 @@
+#R Programming Principles
+
+#integer
+x <- 2
+
+#double
+y<-2.5
+
+#check type
+typeof(y)
+typeof(x)
+
+#complex
+z<-3 + 2i
+
+typeof(z) #complex is returned
+
+#character
+z<="h"
+
+#logical
+a<- T
+a<- TRUE
+
+#Using Variables
+
+var1<-2.5
+var2<-4
+
+result <- var1/var2
+result  #0.625 is returned
+
+#Call functions and pass variables
+answer <- sqrt(var2)
+
