1886219868d8b4ff45e44fa62e73b5bc30551dae
psmall57 <phillip.small@gmail.com>
2019-11-21 14:59:59 -0500

    Update Vectors_Matrices_R.r

--- Vectors_Matrices_R.r
+++ Vectors_Matrices_R.r
@@ -1,5 +1,8 @@
 #Vectors and Matrices
 #Vector Variables
+
+#vectors are containers of data indexed like arrays.  Can perform arithemetic and other functions.
+
  m <-c(1,2,3)
  n <-c(6,5,4)
 # &Vectorized  
@@ -24,4 +27,22 @@
  apply(m,2,sum) #sum of columns
  apply(m,2,sort) #sort of columns
  
- 
+ v<-c(7,10,4,3)
+
+y<-2
+z<- c(y,y *2, y+1,0,v)
+
+a<- v*y
+a
+
+#different size vectors
+a<- c(1,2)
+b<- c(1,2,3,4)
+d<-(a+b)
+d
+
+#seq + rep
+v<- 1:10 #returns 1 thru 10
+v<- seq(5,100,by=5) #returns 5 to 100 in 5 increments
+
+v<-rep(1:5,5) # returns 1 thru 5 repeated 5 times
